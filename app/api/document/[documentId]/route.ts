70225037cd215847bed8618775b84d15b4ac9f15
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-05-21 19:58:46 -0400

    post and start building page

--- app/api/document/[documentId]/route.ts
+++ app/api/document/[documentId]/route.ts
@@ -1,8 +1,8 @@
 import prisma from "@/prisma";
 import { Block } from "@prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (_, { params }: any) => {
+export const GET = async (_: any, { params }: any) => {
   const { documentId } = params;
 
   // This should really have a flow for creating a document first and then adding blocks to it
@@ -17,4 +17,37 @@ export const GET = async (_, { params }: any) => {
   } catch (e) {
     throw new Error("Community not found");
   }
-});
+};
+
+export const POST = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      documentId: string;
+    };
+  }
+) => {
+  const { documentId } = params;
+  const body = await req.json();
+
+  try {
+    const block = await prisma.block.create({
+      data: {
+        documentId: parseInt(documentId),
+        position: body.position,
+        type: body.type,
+        text: body.text,
+      },
+    });
+
+    return NextResponse.json(block);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "An error occurred while creating the block" },
+      { status: 500 }
+    );
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { EllipsisVerticalIcon } from "@heroicons/react/16/solid";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+      <div className="w-full max-w-5xl items-center justify-between text-md">
+        <div className="group w-full flex items-center h-8 rounded-md text-md ">
+          <EllipsisVerticalIcon className="invisible group-hover:visible h-4 w-4" />
+          <input
+            type="text"
+            className="w-full h-full px-2 invisible group-hover:visible"
+            placeholder="Add a line..."
+          />
+        </div>
+      </div>
     </main>
   );
 }
