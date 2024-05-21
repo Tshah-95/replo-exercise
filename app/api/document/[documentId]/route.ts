61c33cb420e21466d007f28ddc70eaf48fbece4b
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-05-21 19:33:43 -0400

    axe boilerplate bs and make basic get route

---
+++ app/api/document/[documentId]/route.ts
@@ -0,0 +1,20 @@
+import prisma from "@/prisma";
+import { Block } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export const GET = async (_, { params }: any) => {
+  const { documentId } = params;
+
+  // This should really have a flow for creating a document first and then adding blocks to it
+  try {
+    const blocks = await prisma.block.findMany({
+      where: {
+        documentId: parseInt(documentId),
+      },
+    });
+
+    return NextResponse.json<Block[]>(blocks);
+  } catch (e) {
+    throw new Error("Community not found");
+  }
+});
